e51192b0829719d57eb2986228deba3c9ca6cf92
Pham Thanh Minh <thanhminh1602@gmail.com>
2021-04-16 09:51:06 +0700

    fix mega menu

--- package-main/assets/js/global.js
+++ package-main/assets/js/global.js
@@ -7,36 +7,56 @@
 ;( function( w, $ ) {
     'use strict';
 
-    var btnSearch = $('.header-search');
-    var btnBackHome = $('.top-search');
+    var searchNav = function(){
+      var btnSearch = $('.header-search');
+      var btnBackHome = $('.top-search');
 
-    btnSearch.on('click',function(){
-      $.magnificPopup.open({
-        items: {
-          src: '#ins_form_search'
-        },
-        type: 'inline',
-        mainClass: 'mfp-with-zoom',
-        zoom: {
-          enabled: true, // By default it's false, so don't forget to enable it
-
-          duration: 300, // duration of the effect, in milliseconds
-          easing: 'ease-in-out', // CSS transition easing function
-
-          // The "opener" function should return the element from which popup will be zoomed in
-          // and to which popup will be scaled down
-          // By defailt it looks for an image tag:
-          opener: function(openerElement) {
-            // openerElement is the element on which popup was initialized, in this case its <a> tag
-            // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-            return openerElement.is('img') ? openerElement : openerElement.find('img');
+      btnSearch.on('click',function(){
+        $.magnificPopup.open({
+          items: {
+            src: '#ins_form_search'
+          },
+          type: 'inline',
+          mainClass: 'mfp-with-zoom',
+          zoom: {
+            enabled: true, // By default it's false, so don't forget to enable it
+            duration: 300, // duration of the effect, in milliseconds
+            easing: 'ease-in-out', // CSS transition easing function
+            opener: function(openerElement) {
+              return openerElement.is('img') ? openerElement : openerElement.find('img');
+            }
           }
-        }
+        });
       });
+
+      btnBackHome.on('click',function(){
+        $('.ins-search-form-default .mfp-close').trigger('click')
+      });
+    }
+
+
+    var megaMenu = function(){
+      var megamenu = $('.wpmm_mega_menu');
+      var screenWidth = $(window).width();
+      $('.style-megamenu').remove();
+      megamenu.each(function( index ) {
+        var x = $(this).offset();
+        var id = $(this).attr('id');
+        var megamenu = $(this).find('.wp-megamenu-sub-menu');
+        var spacingWhite = (screenWidth - megamenu.width()) / 2;
+        var leftArrow = (x.left - (spacingWhite - 100));
+        $('body').append('<div class="style-megamenu"><style>#'+id+'>.wp-megamenu-sub-menu::after{left:'+leftArrow+'px !important}</style></div>');
+      });
+    }
+
+    $(window).on('load',function(){
+      searchNav();
+      megaMenu();
     });
 
-    btnBackHome.on('click',function(){
-      $('.ins-search-form-default .mfp-close').trigger('click')
+    $(window).on('resize',function(){
+      megaMenu();
     });
+
 
 } )( window, jQuery )
