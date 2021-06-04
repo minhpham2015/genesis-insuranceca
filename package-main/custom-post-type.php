d53732863fee607564686e085e7f0204f2a006b6
Pham Thanh Minh <thanhminh1602@gmail.com>
2021-06-04 15:25:01 +0700

    update custom post type

--- package-main/custom-post-type.php
+++ package-main/custom-post-type.php
@@ -131,7 +131,7 @@ function insuranceca_custom_taxonomy(){
 		//Topics
 		register_taxonomy(
 	        'ins-topic',  // The name of the taxonomy. Name should be in slug form (must not contain capital letters or spaces).
-	        array('resources','ins-faqs'), // post type name
+	        array('ins-faqs','resources'), // post type name
 	        array(
 	            'hierarchical' => false,
 	            'label' => 'Topic', // display name
